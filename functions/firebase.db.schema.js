375b54a20dd354a8fda3cdc10186bcf1f17529df
just-be-weird <abhi.chandresh@gmail.com>
2019-07-10 20:57:31 +0530

    Firebase backend setup 1.

---
+++ functions/firebase.db.schema.js
@@ -0,0 +1,10 @@
+let db = {
+    users: [
+        {
+            userHandle: 'user',
+            body: "body",
+            createdAt: "2019-07-09T15:08:28.110Z",
+            likeCount: 0
+        }
+    ]
+}
--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,5 @@
 const functions = require("firebase-functions");
+const firebase = require("firebase");
 const admin = require("firebase-admin");
 const app = require("express")();
 
@@ -6,18 +7,26 @@ admin.initializeApp({ credential: admin.credential.applicationDefault() });
 const firestoreDB = admin.firestore();
 
 app.get("/users", async (req, res) => {
-    const data = await firestoreDB.collection("notebooks").get();
+    const data = await firestoreDB
+        .collection("notebooks")
+        .orderBy("createdAt", "desc")
+        .get();
     let notebooks = [];
     if (data) {
         data.forEach(doc => {
-            notebooks.push(doc.data());
+            notebooks.push({
+                userId: doc.id,
+                body: doc.data().body,
+                userHandel: doc.data().userHandel,
+                createdAt: doc.data().createdAt,
+            });
         });
         res.json(notebooks);
         // response.status(400).json({msg: "Error at get request."});
     }
 });
 
-exports.createUsers = functions.https.onRequest(async (request, response) => {
+app.post("/user", async (request, response) => {
     let notebook = {
         body: request.body.body,
         createdAt: new Date().toISOString(),
@@ -27,4 +36,25 @@ exports.createUsers = functions.https.onRequest(async (request, response) => {
     response.json({ msg: `document ${data.id} created.` });
 });
 
-exports.api = functions.https.onRequest(app);
+//sign-up route
+app.post("/auth/signup", async (req, res) => {
+    const newUser = {
+        email: req.body.email,
+        password: req.body.password,
+        rePassword: req.body.rePassword,
+        handle: req.body.handle,
+    };
+
+    //todo validate data
+    const data = await firebase
+        .auth()
+        .createUserWithEmailAndPassword(newUser.email, newUser.password);
+    if (!data) {
+        return res.status(401).json({ message: "invalid credentials" });
+    }
+    res.status(201).json({
+        message: `user ${data.user.uid} signed up successfully.`
+    });
+});
+
+exports.api = functions.region("asia-east2").https.onRequest(app);
