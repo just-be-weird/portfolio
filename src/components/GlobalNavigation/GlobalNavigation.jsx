4a69be50e04dd1e45c62d385955fd504f50d6c41
just-be-weird <abhi.chandresh@gmail.com>
2019-08-25 19:08:15 +0530

    Major UI fix for mobile devices
    Signed-off-by: just-be-weird <abhi.chandresh@gmail.com>

--- src/components/GlobalNavigation/GlobalNavigation.jsx
+++ src/components/GlobalNavigation/GlobalNavigation.jsx
@@ -1,12 +1,16 @@
-import React, { Fragment, useEffect } from 'react'
-import { Link } from 'react-router-dom'
-import classes from '../Sass/main.module.scss';
+import React, { Fragment, useEffect } from "react";
+import { Link } from "react-router-dom";
+import classes from "../Sass/main.module.scss";
 
 export default function GlobalNavigation() {
     useEffect(() => {
-        if (!/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        if (
+            !/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+                navigator.userAgent
+            )
+        ) {
             let scrollDirection = 0;
-            window.addEventListener('scroll', (e) => {
+            window.addEventListener("scroll", e => {
                 let winTop = window.scrollY,
                     global__nav = document.getElementById("global__nav");
                 if (winTop > scrollDirection) {
@@ -22,91 +26,129 @@ export default function GlobalNavigation() {
                     return false;
                 }
                 scrollDirection = winTop;
-            })
+            });
         }
-    }, [])
+    }, []);
 
     const clickHandler = e => {
         e.preventDefault();
         document.getElementById("navi-toggle").checked = false;
-    }
+    };
     const data = [
         {
             id: "home",
             nav_title: "Home",
             nav_to: "/",
             nav_class: "",
-            svg: false
-        }
-        , {
+            svg: false,
+        },
+        {
             id: "about",
             nav_title: "About",
             nav_to: "/about",
             nav_class: "",
-            svg: false
-        }
-        , {
+            svg: false,
+        },
+        {
             id: "experience",
             nav_title: "Experience",
             nav_class: "",
             nav_to: "/experience",
-            svg: false
-        }
-        , {
+            svg: false,
+        },
+        {
             id: "stones",
             nav_title: "Stones",
             nav_to: "/stones",
             nav_class: "main-nav-box__infinite",
             svg: true,
-            svg_icon: "/assets/img/sprite.svg#icon-infinite"
-        }
-        , {
+            svg_icon: "/assets/img/sprite.svg#icon-infinite",
+        },
+        {
             id: "projects",
             nav_title: "Projects",
             nav_to: "/projects",
             svg: false,
-            nav_class: ""
-        }
-        , {
+            nav_class: "",
+        },
+        {
             id: "contact",
             nav_title: "Contact",
             nav_to: "/contact",
-            nav_class: ""
-        }
+            nav_class: "",
+        },
     ];
 
-    const jsx = (
-        data.length > 0 && data.map(nav_item => (
-            <li key={nav_item.id} className={classes["navigation__item"] + (nav_item.nav_class.length > 0 ? " " + classes[nav_item.nav_class] : "")} onClick={e => clickHandler(e)}>
-                <Link className={classes["navigation__link"] + (nav_item.nav_class.length > 0 ? " " + classes[nav_item.nav_class] : "")} to={nav_item.nav_to}>
-                    {
-                        nav_item.svg && (
-                            <svg className={classes[`${nav_item.nav_class}-icon`]}>
-                                <use xlinkHref={nav_item.svg_icon}></use>
-                            </svg>)
+    const jsx =
+        data.length > 0 &&
+        data.map(nav_item => (
+            <li
+                key={nav_item.id}
+                className={
+                    classes["navigation__item"] +
+                    (nav_item.nav_class.length > 0
+                        ? " " + classes[nav_item.nav_class]
+                        : "")
+                }
+                onClick={e => clickHandler(e)}
+            >
+                <Link
+                    className={
+                        classes["navigation__link"] +
+                        (nav_item.nav_class.length > 0
+                            ? " " + classes[nav_item.nav_class]
+                            : "")
                     }
+                    to={nav_item.nav_to}
+                >
+                    {nav_item.svg && (
+                        <svg className={classes[`${nav_item.nav_class}-icon`]}>
+                            <use xlinkHref={nav_item.svg_icon}></use>
+                        </svg>
+                    )}
 
-                    {nav_item.nav_title}</Link>
+                    {nav_item.nav_title}
+                </Link>
             </li>
-        ))
-    )
+        ));
     return (
         <Fragment>
-            <input type="checkbox" name="navi-toggle" className={classes["navigation__checkbox"]} id="navi-toggle" />
-            <label htmlFor="navi-toggle" className={classes["navigation__button"]}>
+            <input
+                type='checkbox'
+                name='navi-toggle'
+                className={classes["navigation__checkbox"]}
+                id='navi-toggle'
+            />
+            <label
+                htmlFor='navi-toggle'
+                className={classes["navigation__button"]}
+            >
                 <span className={classes["navigation__icon"]}>&nbsp;</span>
             </label>
             <div className={classes["navigation__background"]}>&nbsp;</div>
-            <nav className={classes["navigation"] + " " + classes["navigation__nav"] + " " + classes['landing-page']} id='global__nav'>
+            <nav
+                className={
+                    classes["navigation"] + " " + classes["navigation__nav"]
+                }
+                id='global__nav'
+            >
                 <div className={classes["row"]}>
                     <svg className={classes["just-be-weird-logo"]}>
-                        <use xlinkHref={"/assets/img/sprite.svg#icon-flickr4"}></use>
+                        <use
+                            xlinkHref={"/assets/img/sprite.svg#icon-flickr4"}
+                        ></use>
                     </svg>
-                    <ul className={classes["main-nav"] + " " + classes["navigation__list"]}>
+                    <ul
+                        className={
+                            classes["main-nav"] +
+                            " " +
+                            classes["navigation__list"]
+                        }
+                    >
                         {jsx}
                     </ul>
                 </div>
             </nav>
         </Fragment>
-    )
+    );
 }
