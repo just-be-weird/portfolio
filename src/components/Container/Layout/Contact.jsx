1f763c4619365f5d198db6d45c1d9d823a73ac52
just-be-weird <abhi.chandresh@gmail.com>
2019-08-26 14:14:10 +0530

    Improved contact page ui
    Signed-off-by: just-be-weird <abhi.chandresh@gmail.com>

--- src/components/Container/Layout/Contact.jsx
+++ src/components/Container/Layout/Contact.jsx
@@ -1,32 +1,51 @@
 import React from "react";
-import classes from '../../Sass/main.module.scss'
+import classes from "../../Sass/main.module.scss";
 
 export default function Contact() {
-  return (
-    <section className={classes["section-contact"]}>
-      <div className={classes["section-wrapper"]} id="contact">
-        <h2 className={classes["section-title"]}>Contact</h2>
-        <p className={classes["long-copy"]}>
-          Let`s build something unique. If you've some cool project, ideas or
-          just want to have a talk, then feel free to contact me anytime. I will
-          be glad<span role="img" aria-label='happy-simile'>😊</span> to hear.
-        </p>
-        <div className={classes["cta-container"]}>
-          <a
-            className={classes["btn"]+" "+classes["btn--blue"]}
-            href="mailto:abhi.chandresh@gmail.com"
-          >
-            Say Hi <span role="img" aria-label='hi-gesture'> 👋</span>
-          </a>
-          <a
-            className={classes["btn"]+" "+classes["btn--blue"]+" "+classes["btn-resume"]}
-            target="_blank"
-            href="public/assets/docs/AbhishekPrajpati_Resume_2019_07.pdf"
-          >
-            Resume
-          </a>
-        </div>
-      </div>
-    </section>
-  );
+    return (
+        <section className={classes["section-contact"]}>
+            <img
+                className={classes.contact__image}
+                src='/assets/img/contact.svg'
+                alt='UserAuthPage'
+            />
+            <div className={classes["section-wrapper"]} id='contact'>
+                <h2 className={classes["section-title"]}>Contact</h2>
+                <p className={classes["long-copy"]}>
+                    Let`s build something unique. If you've some cool project,
+                    ideas or just want to have a talk, then feel free to contact
+                    me anytime. I will be glad
+                    <span role='img' aria-label='happy-simile'>
+                        😊
+                    </span>{" "}
+                    to hear.
+                </p>
+                <div className={classes["cta-container"]}>
+                    <a
+                        className={classes["btn"] + " " + classes["btn--blue"]}
+                        href='mailto:abhi.chandresh@gmail.com'
+                    >
+                        Say Hi{" "}
+                        <span role='img' aria-label='hi-gesture'>
+                            {" "}
+                            👋
+                        </span>
+                    </a>
+                    <a
+                        className={
+                            classes["btn"] +
+                            " " +
+                            classes["btn--blue"] +
+                            " " +
+                            classes["btn-resume"]
+                        }
+                        target='_blank'
+                        href='public/assets/docs/AbhishekPrajpati_Resume_2019_07.pdf'
+                    >
+                        Resume
+                    </a>
+                </div>
+            </div>
+        </section>
+    );
 }
