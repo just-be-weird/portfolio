85d739ec3cda3d7bfdacf5b827f8e9f198a8eb29
just-be-weird <abhi.chandresh@gmail.com>
2019-09-23 18:57:27 +0530

    Refactored the Dashboard comp into modular comps
    Signed-off-by: just-be-weird <abhi.chandresh@gmail.com>

---
+++ src/components/Portfolio/Portfolios.jsx
@@ -0,0 +1,62 @@
+import React, { useEffect, useState } from "react";
+import PropTypes from "prop-types";
+import axios from "../../axios.instance";
+import { connect } from "react-redux";
+import classes from "../Sass/main.module.scss";
+
+const Portfolios = ({ handle: activeHandle }) => {
+    let [portfolios, setPortfolioData] = useState([]);
+    useEffect(() => {
+        axios.get(`/notebook/all`).then(res => {
+            setPortfolioData(res.data);
+        });
+    }, [setPortfolioData]);
+
+    const jsx =
+        portfolios.length > 0 &&
+        portfolios.map(portfolio => {
+            const { notebookId, userImage, handle, followerCount } = portfolio;
+            console.log(">>", activeHandle  ,handle);
+            const {
+                portfolio__content,
+                portfolio__row,
+                portfolio__user_img,
+                portfolio__title,
+            } = classes;
+            //filter out the logged in user from all portfolios
+            return !activeHandle ? null : activeHandle !== handle && (
+                <div key={notebookId} className={portfolio__row}>
+                    <div className={portfolio__user_img}>
+                        <a href={`/portfolio/${handle}`}>
+                            <img src={userImage} alt='User' />
+                        </a>
+                    </div>
+                    <div className={portfolio__content}>
+                        <h4 className={portfolio__title}>
+                            <a href={`/portfolio/${handle}`}>{handle}</a>
+                        </h4>
+                        <p>Followers: {followerCount}</p>
+                    </div>
+                </div>
+            );
+        });
+
+    return (
+        <section className={classes.wrap_container}>
+            <div>
+                <h2 className={classes["section-title"]}>Portfolios</h2>
+            </div>
+            <div className={classes.row}>{jsx}</div>
+        </section>
+    );
+};
+
+Portfolios.propTypes = {
+    handle: PropTypes.string,
+};
+const mapStateToProps = state => ({
+    handle: state.profile.handle,
+});
+
+
+export default connect(mapStateToProps,{})(Portfolios);
---
+++ src/components/Portfolio/UserPortfolio.jsx
@@ -0,0 +1,126 @@
+import React, { Fragment, useState } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import axios from "../../axios.instance";
+import { loadingUI, setUIErrors } from "../actions/ui";
+import { setProfile } from "../actions/profile";
+import Portfolio from "./Portfolio";
+import Modal from "../UI/Modal/Modal";
+import CustomInput from "../UI/InputButtons/CustomInput";
+import { matchAnyChar } from "../Shared/Util";
+import classes from "../Sass/main.module.scss";
+
+const UserPortfolio = ({
+    setProfile,
+    profile,
+    loadingUI,
+    setUIErrors,
+    match,
+}) => {
+    const [hideModal, setHideModal] = useState(false);
+    const [editMdl, setEditMdl] = useState({});
+
+    const {
+        params: { userHandle },
+    } = match;
+
+    const { btn } = classes;
+
+    const editHandler = async e => {
+        e.stopPropagation();
+        setHideModal(!hideModal);
+        setEditMdl({
+            label: e.currentTarget.attributes["data-action"].value,
+        });
+    };
+    
+    const submitHandler = async e => {
+        e.preventDefault();
+        if (!editMdl.val) {
+            setUIErrors({
+                error: `User ${editMdl.label} is required.`.toUpperCase(),
+            });
+        } else {
+            if (editMdl.editing) {
+                loadingUI(true);
+                const res = await axios.post(`/notebook/profile`, {
+                    [editMdl.label]: editMdl.val,
+                });
+
+                setProfile({
+                    ...profile,
+                    [editMdl.label]: res.data.data[editMdl.label],
+                });
+                setHideModal(false);
+                loadingUI();
+            } else {
+                setProfile({ ...profile });
+            }
+        }
+    };
+
+    const changeHandler = e => {
+        setEditMdl({
+            ...editMdl,
+            val: e.target.value,
+            editing: true,
+        });
+    };
+
+    return (
+        <Fragment>
+            <Modal
+                modalState={hideModal}
+                hideModal={() => setHideModal(false)}
+                id={"user-details_modal"}
+            >
+                <CustomInput
+                    iptype='text'
+                    ipid='user-details'
+                    htmlFor='user-details'
+                    placeholderVal={`Enter Value For User ${editMdl.label}`}
+                    regEx={matchAnyChar(4, 100)}
+                    labelName={`User ${editMdl.label}`.toUpperCase()}
+                    isRequired={true}
+                    val={
+                        !editMdl.editing ? profile[editMdl.label] : editMdl.val
+                    }
+                    changeHandler={changeHandler}
+                />
+                <div className={classes["btn-wrapper"]}>
+                    <div
+                        className={btn + " " + classes["btn--arrow"]}
+                        onClick={submitHandler}
+                    >
+                        <svg className={classes["svg-arrow"]}>
+                            <use xlinkHref={"/assets/img/sprite.svg#arrow"} />
+                        </svg>
+                    </div>
+                </div>
+            </Modal>
+            <Portfolio
+                profile={profile}
+                userHandle={userHandle}
+                setProfile={setProfile}
+                loadingUI={loadingUI}
+                editHandler={editHandler}
+                setUIErrors={setUIErrors}
+            />
+        </Fragment>
+    );
+};
+
+UserPortfolio.propTypes = {
+    loadingUI: PropTypes.func.isRequired,
+    setUIErrors: PropTypes.func.isRequired,
+    setProfile: PropTypes.func.isRequired,
+    profile: PropTypes.object.isRequired,
+};
+const mapStateToProps = state => ({
+    profile: state.profile,
+});
+
+export default connect(
+    mapStateToProps,
+    { loadingUI, setUIErrors, setProfile }
+)(UserPortfolio);
